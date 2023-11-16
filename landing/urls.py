b8c4f7918d22182029b76b32bff896ff6363c075
Tariq Hosny <49675968+tariqhosny@users.noreply.github.com>
2023-11-17 00:16:45 +0200

    add landing page

---
+++ landing/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing, name="index"),
+    # path('result', views.results, name="result"),
+]
--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import GetResultsForm
+# from importData.models import Grade, Student
 
 # Create your views here.
+
+def landing(request):
+    return render(request, 'landing/landing.html')
+
+# def results(request):
+#     form = {'form': GetResultsForm}
+#     try:
+#         national_id = request.POST.get('nationalID')
+#         if national_id != None:
+#             student = Student.objects.get(national_id = national_id)
+#             grades = Grade.objects.all().filter(student = student)
+#             form['grades'] = grades
+#     except:
+#         print("error")
+    
+#     return render(request, 'landing/result-form.html', form)
