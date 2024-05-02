acac8b5dd5f22575a641e8e2259e6db633d0a24e
Tariq Hosny <tariqhosny@gmail.com>
2024-05-02 15:47:50 +0300

    add the minimum amount to the parts field

--- registration/forms.py
+++ registration/forms.py
@@ -15,14 +15,36 @@ class SubmitNewStudentForm(forms.ModelForm):
         fields = ['national_id', 'name', 'phone', 'part', 'soura']
     
     def __init__(self, *args, **kwargs):
+        min_amount = kwargs.pop('min_amount', None)
+        parts = Part.objects.all()
+        if min_amount is not None:
+            parts = Part.objects.filter(number__gt=(min_amount-1))
         super().__init__(*args, **kwargs)
+
         self.fields['part'].label = "عدد الاجزاء"
         self.fields['soura'].label = 'السورة'
         self.fields['part'].widget.attrs.update({'id':'part', 'name':'part'})
         self.fields['soura'].widget.attrs.update({'id':'soura', 'name':'soura'})
         self.fields['soura'].queryset = Soura.objects.none()
+        self.fields['part'].queryset = parts
+
+        self.fields['name'].error_messages = {
+            'required': 'لازم تدخل الاسم',
+        }
+        self.fields['phone'].error_messages = {
+            'required': 'لازم تدخل رقم التليفون',
+        }
+        self.fields['part'].error_messages = {
+            'required': 'لازم تختار عدد الاجزاء',
+        }
+        self.fields['soura'].error_messages = {
+            'required': 'لازم تختار سورة',
+        }
 
         if 'part' in self.data:
-            part_id = int(self.data.get('part'))
-            part = Part.objects.get(pk=part_id)
-            self.fields["soura"].queryset = part.soura.all()
+            try:
+                part_id = int(self.data.get('part'))
+                part = Part.objects.get(pk=part_id)
+                self.fields["soura"].queryset = part.soura.all()
+            except:
+                print('error')
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
 from django.http import JsonResponse
-from importData.models import Student, Part
+from importData.models import Student, Part, Soura
 from .forms import NationalIDForm, SubmitNewStudentForm
 
 # Create your views here.
@@ -9,9 +9,11 @@ form = {'nationalIDForm': NationalIDForm}
 form['hideNationalID'] = False
 form['nationalID'] = None
 form['student'] = None
+form['min_amount'] = None
 
 @csrf_protect
 def registration(request):
+    print(form['min_amount'])
     if request.method == "POST":
         if 'nationalIDForm' in request.POST:
             national_id = request.POST.get('nationalID')
@@ -20,14 +22,15 @@ def registration(request):
             if national_id != None:
                 try:
                     student = Student.objects.get(national_id = national_id)
+                    form['min_amount'] = student.next_amount.number
                     form['student'] = student
-                    form['form'] = SubmitNewStudentForm(initial={'national_id': form['nationalID'], 'name': student.name, 'phone': student.phone})
+                    form['form'] = SubmitNewStudentForm(initial={'national_id': form['nationalID'], 'name': student.name, 'phone': student.phone}, min_amount=form['min_amount'])
                 except:
                     form['student'] = None
                     form['form'] = SubmitNewStudentForm(initial={'national_id': form['nationalID']})
         elif 'submitForm' in request.POST:
             form['hideNationalID'] = True
-            submitForm = SubmitNewStudentForm(request.POST)
+            submitForm = SubmitNewStudentForm(request.POST, min_amount=form['min_amount'])
             form['form'] = submitForm
             if submitForm.is_valid():
                 submitForm.save()
@@ -38,8 +41,16 @@ def registration(request):
     return render(request, 'registration/registration-form.html', form)
 
 def loadSoura(request):
-    part_id = request.GET.get('part')
-    part = Part.objects.get(pk=part_id)
-    souras = part.soura.values('id', 'title')
+    try:
+        part_id = request.GET.get('part')
+        part = Part.objects.get(pk=part_id)
+        souras = part.soura.values('id', 'title')
+    except:
+        souras = Soura.objects.none()
+    # if part_id is None:
+    #     souras = Soura.objects.none
+    # else:
+    #     part = Part.objects.get(pk=part_id)
+    #     souras = part.soura.values('id', 'title')
     data = [{'id': soura['id'], 'name': soura['title']} for soura in souras]
     return JsonResponse(data, safe=False)
