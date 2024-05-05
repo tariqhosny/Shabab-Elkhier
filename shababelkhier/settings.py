8bac92ae0c5945f4fd4ac7ffbdc1846e06936116
Tariq Hosny <tariqhosny@gmail.com>
2024-05-05 16:18:17 +0300

    edit middleware to registration

--- shababelkhier/settings.py
+++ shababelkhier/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'registration.middleware.CSRFMiddleware', 
+    'registration.custom_middleware.CSRFMiddleware', 
 ]
 
 ROOT_URLCONF = 'shababelkhier.urls'
