97ded2ec700ac2add526e028bc58dccea2755c0c
Tariq Hosny <tariqhosny@gmail.com>
2024-05-06 00:23:43 +0300

    update the buttons colors and some UI issues

--- landing/forms.py
+++ landing/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 
 class GetResultsForm(forms.Form):
-    nationalID = forms.IntegerField(max_value=32412319999999, label="", widget=forms.TextInput(attrs={'placeholder': 'ادخل الرقم القومي'}))
+    nationalID = forms.CharField(max_length=14, min_length=14, label="الرقم القومي", widget=forms.TextInput(attrs={'placeholder': 'ادخل الرقم القومي'}))
     # nationalID = forms.CharField(max_length=14, label="", widget=forms.TextInput(attrs={'placeholder': 'ادخل الرقم القومي'}))
-
+    def clean_nationalID(self):
+        data = self.cleaned_data['nationalID']
+        if not data.isdigit() or len(data) != 14:
+            raise forms.ValidationError('لازم تدخل الرقم القومي بشكل صحيح 14 رقم')
+        return data
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nationalID'].error_messages = {
+            'required': 'لازم تدخل الرقم القومي',
+            'invalid': 'لازم تدخل الرقم القومي بشكل صحيح 14 رقم',
+            'min_length': 'لازم تدخل الرقم القومي بشكل صحيح 14 رقم',
+            'min_length': 'لازم تدخل الرقم القومي بشكل صحيح 14 رقم',
+        }
--- landing/views.py
+++ landing/views.py
@@ -18,26 +18,29 @@ def landing(request):
 
 @csrf_protect
 def results(request):
-    form = {'form': GetResultsForm}
+    form = {'nationalIDForm': GetResultsForm}
     form['grades'] = []
     sour = []
-    try:
-        national_id = request.POST.get('nationalID')
-        if national_id != None:
-            student = Student.objects.get(national_id = national_id)
-            grades = Grade.objects.all().filter(student = student)
-            form['grades'] = grades
-            for grade in grades:
-                if (grade.from_baqra):
-                    sour.append('من سورة الفاتحة الي سورة ' + grade.soura.title)
-                else:
-                    sour.append(grade.soura.title)
-            form['sour'] = sour
-    except:
-        form['grades'] = None
-        print("error")
+    if request.method == "POST":
+        form['nationalIDForm'] = GetResultsForm(request.POST)
+        if form['nationalIDForm'].is_valid():
+            try:
+                national_id = request.POST.get('nationalID')
+                if national_id != None:
+                    student = Student.objects.get(national_id = national_id)
+                    grades = Grade.objects.all().filter(student = student)
+                    form['grades'] = grades
+                    for grade in grades:
+                        if (grade.from_baqra):
+                            sour.append('من سورة الفاتحة الي سورة ' + grade.soura.title)
+                        else:
+                            sour.append(grade.soura.title)
+                    form['sour'] = sour
+            except:
+                form['grades'] = None
+                print("error")
     
-    return render(request, 'landing/result-form.html', form)
+    return render(request, 'result/result-form.html', form)
 
 
 #error handling
