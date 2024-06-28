7abad0122a865bcf24c8f45d61a6087c69b8094c
Tariq Hosny <tariqhosny@gmail.com>
2024-06-28 19:47:44 +0300

    reset the request after post it

--- shababelkhier/settings.py
+++ shababelkhier/settings.py
@@ -33,12 +33,6 @@ ALLOWED_HOSTS = [
     'shababtest.pythonanywhere.com',
     ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://127.0.0.1',
-    'https://tariqhosny.pythonanywhere.com',
-    'https://shababelkhier.pythonanywhere.com',
-    'https://shababtest.pythonanywhere.com',
-    ]
 # Application definition
 
 INSTALLED_APPS = [
