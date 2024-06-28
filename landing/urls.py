10203786a963db3216ce3a14e6ce90b2d0628c4b
Tariq Hosny <tariqhosny@gmail.com>
2024-06-28 19:27:18 +0300

    check cookies

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import handler403
 
 urlpatterns = [
     path('', views.landing, name="index"),
     path('result', views.results, name="result"),
 ]
+
+handler403 = views.handler403
