2dc40a97bf592300b7cdb3d390dd550d4a690de6
Tariq Hosny <tariqhosny@gmail.com>
2024-06-28 19:10:37 +0300

    add javascript function to reload the page

--- middleware/custom_csrf_middleware.py
+++
@@ -1,8 +0,0 @@
-# middleware.py
-from django.middleware.csrf import CsrfViewMiddleware
-from django.shortcuts import render
-from django.utils.deprecation import MiddlewareMixin
-
-class CustomCsrfViewMiddleware(CsrfViewMiddleware):
-    def _reject(self, request, reason):
-        return render(request, 'errors.error.html', {'reason': reason})
--- shababelkhier/settings.py
+++ shababelkhier/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.custom_csrf_middleware.CustomCsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'shababelkhier.urls'
